c4a02bf35520dba6ecc7137f61eddf030ee881c2
Kofi_Osei <fiko8464@gmail.com>
2023-07-14 01:54:39 +0100

    done

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - print a sentence
+ * Return: 0 always
+ */
+int main(void)
+{
+	printf("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
